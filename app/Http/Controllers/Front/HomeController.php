08791509ef42c98add3fa0e3752c44dd17c7c720
Mohamed Eid <m.mohamedeid11@gmail.com>
2024-05-10 19:39:13 +0300

    finishing the first section

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -5,14 +5,12 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Modules\Category\Models\Category;
-use Modules\Product\Models\Product;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $categories = Category::active()->limit(3)->get();
-        $products = Product::active()->limit(3)->get();
         return view('themes.theme1.index' , get_defined_vars());
     }
 
