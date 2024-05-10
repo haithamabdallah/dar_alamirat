68aa6bb82cb7c4db931e60b1ed87ca7f5617fea6
Mohamed Eid <m.mohamedeid11@gmail.com>
2024-05-10 19:11:32 +0300

    make the front dynamic version 2

--- resources/views/themes/theme1/index-layouts/categoryTabs.blade.php
+++ resources/views/themes/theme1/index-layouts/categoryTabs.blade.php
@@ -3,29 +3,57 @@
         <div class="wrap">
             <div class="tabs-contents">
                 <ul class="nav nav-pills">
-                    <li class="nav-item">
-                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab"
-                                data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane"
-                                aria-selected="true">Latest Products
-                        </button>
-                    </li>
-                    <li class="nav-item">
-                        <button class="nav-link" id="profile-tab" data-bs-toggle="tab"
-                                data-bs-target="#profile-tab-pane" type="button" role="tab"
-                                aria-controls="profile-tab-pane" aria-selected="false">Most Sales
-                        </button>
-                    </li>
-                    <li class="nav-item">
-                        <button class="nav-link" id="contact-tab" data-bs-toggle="tab"
-                                data-bs-target="#contact-tab-pane" type="button" role="tab"
-                                aria-controls="contact-tab-pane" aria-selected="false">Editor Choice
-                        </button>
-                    </li>
+                    @foreach($categories as $category)
+                        <li class="nav-item">
+                            <button class="nav-link active" id="home-tab" data-bs-toggle="tab"
+                                    data-bs-target="#home-tab-{{$category->id}}" type="button" role="tab" aria-controls="home-tab-{{$category->id}}"
+                                    aria-selected="true"> {{$category->name}}
+                            </button>
+                        </li>
+                    @endforeach
+                    {{--                    <li class="nav-item">--}}
+                    {{--                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab"--}}
+                    {{--                                data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane"--}}
+                    {{--                                aria-selected="true">Latest Products--}}
+                    {{--                        </button>--}}
+                    {{--                    </li>--}}
+                    {{--                    <li class="nav-item">--}}
+                    {{--                        <button class="nav-link" id="profile-tab" data-bs-toggle="tab"--}}
+                    {{--                                data-bs-target="#profile-tab-pane" type="button" role="tab"--}}
+                    {{--                                aria-controls="profile-tab-pane" aria-selected="false">Most Sales--}}
+                    {{--                        </button>--}}
+                    {{--                    </li>--}}
+                    {{--                    <li class="nav-item">--}}
+                    {{--                        <button class="nav-link" id="contact-tab" data-bs-toggle="tab"--}}
+                    {{--                                data-bs-target="#contact-tab-pane" type="button" role="tab"--}}
+                    {{--                                aria-controls="contact-tab-pane" aria-selected="false">Editor Choice--}}
+                    {{--                        </button>--}}
+                    {{--                    </li>--}}
                 </ul>
 
                 <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab"
-                         tabindex="0">
+                    @foreach($products as $product)
+                        <div class="tab-pane fade show active" id="home-tab-{{$product->category->id}}" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+                            <div class="swiper latestProducts">
+                                <div class="swiper-wrapper">
+                                    <div class="swiper-slide">
+                                        @include("themes.theme1.blocks.items.item-01")
+                                    </div>
+                                    <div class="swiper-slide">@include ("themes.theme1.blocks.items.item-02")</div>
+                                    <div class="swiper-slide">@include ("themes.theme1.blocks.items.item-03")</div>
+                                    <div class="swiper-slide">@include ("themes.theme1.blocks.items.item-04")</div>
+                                    <div class="swiper-slide">@include ("themes.theme1.blocks.items.item-05")</div>
+                                    <div class="swiper-slide">@include ("themes.theme1.blocks.items.item-06")</div>
+                                    <div class="swiper-slide">@include ("themes.theme1.blocks.items.item-07")</div>
+                                    <div class="swiper-slide">@include ("themes.theme1.blocks.items.item-08")</div>
+                                </div>
+                            </div>
+                            <div class="tab-latest-prev"><i class="fa-solid fa-chevron-left"></i></div>
+                            <div class="tab-latest-next"><i class="fa-solid fa-chevron-right"></i></div>
+                        </div>
+                    @endforeach
+
+                    <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
                         <div class="swiper latestProducts">
                             <div class="swiper-wrapper">
                                 <div class="swiper-slide">
