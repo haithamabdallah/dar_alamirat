dfcd266b7e1602de451d543abbf1f72c184063c2
Mohamed Eid <m.mohamedeid11@gmail.com>
2024-05-27 18:12:37 +0300

    pagination the product data - 4

--- resources/views/themes/theme1/category.blade.php
+++ resources/views/themes/theme1/category.blade.php
@@ -440,7 +440,7 @@
                             @endforeach
                         </div>
                         <div class="d-flex justify-content-center">
-                            <button id="load-more" class="s-infinite-scroll-btn s-button-btn s-button-primary" data-url="{{ route('category.products', $category->id) }}" data-page="{{ $products->currentPage() + 1 }}">
+                            <button id="load-more" class="s-infinite-scroll-btn s-button-btn s-button-primary">
                                 Load More
                             </button>
                         </div>
@@ -455,112 +455,91 @@
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
-            const loadMoreButton = document.getElementById('load-more');
-            const productContainer = document.getElementById('product-container');
+            let loadMoreButton = document.getElementById('load-more');
+            let nextPageUrl = "{{ $products->nextPageUrl() }}"; // Initialize with the next page URL
 
             loadMoreButton.addEventListener('click', function () {
-                const url = productContainer.getAttribute('data-url');
-                const nextPage = loadMoreButton.getAttribute('data-page');
-
-                if (!url) {
-                    console.error('Data URL is not defined on the product container.');
-                    return;
-                }
-
-                axios.get(url, {
-                    params: {
-                        page: nextPage
-                    }
-                })
-                    .then(response => {
-                        console.log('Response:', response);
-                        const products = response.data.products;
-                        const nextPageUrl = response.data.nextPage;
-
-                        if (!products) {
-                            console.error('Products data is not defined:', response.data);
-                            return;
-                        }
-
-                        products.forEach(product => {
-                            const productItem = document.createElement('div');
-                            productItem.classList.add('item');
-
-                            // Construct the product HTML using the data from the response
-                            productItem.innerHTML = `
-                    <div class="item-tags">
-                        <span>most popular</span>
-                    </div>
-                    <div class="img">
-                        <a href="${product.url}">
-                            <img class="w-full object-contain" src="${product.thumbnail}" alt="Product Image">
-                        </a>
-                    </div>
-                    <div class="item-data">
-                        <div class="item-price">
-                            ${product.discount_value > 0 ? `
-                            <h4 class="before-dis">
-                                <strong>${product.variants[0].price}</strong>
-                                <span>SAR</span>
-                            </h4>` : ''}
-                            <h4 class="after-dis">
-                                <strong>${product.variants[0].price_with_discount}</strong>
-                                <span>SAR</span>
-                            </h4>
-                            <div class="add-favourite">
-                                <button class="icon-fav">
-                                    <i class="sicon-heart"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="item-dec">
-                            <a href="${product.url}">
-                                <span>${product.title}</span>
-                            </a>
-                        </div>
-                        <button class="tocart add-to-cart button--submit" data-title="Add to Cart">
-                            <span class="button-title"></span>
-                            <i class="sicon-shopping button-icon icon-tocart" data-icon="tocart"></i>
-                            <span class="button-icon icon-wait" data-icon="tocart" style="display: none;">
-                                <svg width="24" height="24" viewBox="0 0 24 24">
-                                    <path d="M19,8L15,12H18A6,6 0 0,1 12,18C11,18 10.03,17.75 9.2,17.3L7.74,18.76C8.97,19.54 10.43,20 12,20A8,8 0 0,0 20,12H23M6,12A6,6 0 0,1 12,6C13,6 13.97,6.25 14.8,6.7L16.26,5.24C15.03,4.46 13.57,4 12,4A8,8 0 0,0 4,12H1L5,16L9,12"></path>
-                                </svg>
-                            </span>
-                            <span class="button-icon icon-success" style="display: none;" data-icon="tocart">
-                                <svg width="24" height="24" viewBox="0 0 24 24">
-                                    <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"></path>
-                                </svg>
-                            </span>
-                        </button>
-                    </div>
-                `;
-
-                            productContainer.appendChild(productItem);
-                        });
-
-                        if (nextPageUrl) {
-                            loadMoreButton.setAttribute('data-page', parseInt(nextPage) + 1);
-                        } else {
-                            loadMoreButton.style.display = 'none';
-                        }
+                if (nextPageUrl) {
+                    fetch(nextPageUrl, {
+                        headers: {
+                            'X-Requested-With': 'XMLHttpRequest',
+                        },
                     })
-                    .catch(error => {
-                        console.error('Error loading more products:', error);
-                        if (error.response) {
-                            // The request was made and the server responded with a status code
-                            console.error('Error response data:', error.response.data);
-                            console.error('Error response status:', error.response.status);
-                            console.error('Error response headers:', error.response.headers);
-                        } else if (error.request) {
-                            // The request was made but no response was received
-                            console.error('Error request:', error.request);
-                        } else {
-                            // Something happened in setting up the request that triggered an Error
-                            console.error('Error message:', error.message);
-                        }
-                    });
+                        .then(response => response.json())
+                        .then(data => {
+                            let productsContainer = document.querySelector('.products-container'); // Container holding the products
+                            data.products.forEach(product => {
+                                let productHtml = `
+                        <!-- product item -->
+                        <div class="item">
+                            <!-- tags -->
+                            <div class="item-tags">
+                                <span>most popular</span>
+                            </div>
+                            <!-- ./tags -->
+                            <!-- img -->
+                            <div class="img">
+                                <a href="{{route('cart-empty')}}">
+                                    <img class="w-full object-contain" src="${product.thumbnail}" alt="Product Image">
+                                </a>
+                            </div>
+                            <!-- img -->
+
+                            <!-- data -->
+                            <div class="item-data">
+                                <!-- price -->
+                                <div class="item-price">
+                                    ${product.discount_value > 0 ? `<h4 class="before-dis"><strong>${product.variants[0].price}</strong><span>SAR</span></h4>` : ''}
+                                    <h4 class="after-dis"><strong>${product.variants[0].price_with_discount}</strong><span>SAR</span></h4>
+                                    <div class="add-favourite">
+                                        <button class="icon-fav">
+                                            <i class="sicon-heart"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <!-- ./price -->
+
+                                <!-- description -->
+                                <div class="item-dec">
+                                    <a href="{{route('cart-empty')}}">
+                                        <span>${product.title}</span>
+                                    </a>
+                                </div>
+                                <!-- ./description -->
+
+                                <!-- button cart -->
+                                <button class="tocart add-to-cart button--submit" data-title="Add to Cart">
+                                    <span class="button-title"></span>
+                                    <i class="sicon-shopping button-icon icon-tocart" data-icon="tocart"></i>
+                                    <span class="button-icon icon-wait" data-icon="tocart" style="display: none;">
+                                        <svg width="24" height="24" viewBox="0 0 24 24">
+                                            <path d="M19,8L15,12H18A6,6 0 0,1 12,18C11,18 10.03,17.75 9.2,17.3L7.74,18.76C8.97,19.54 10.43,20 12,20A8,8 0 0,0 20,12H23M6,12A6,6 0 0,1 12,6C13,6 13.97,6.25 14.8,6.7L16.26,5.24C15.03,4.46 13.57,4 12,4A8,8 0 0,0 4,12H1L5,16L9,12"></path>
+                                        </svg>
+                                    </span>
+                                    <span class="button-icon icon-success" style="display: none;" data-icon="tocart">
+                                        <svg width="24" height="24" viewBox="0 0 24 24">
+                                            <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"></path>
+                                        </svg>
+                                    </span>
+                                </button>
+                                <!-- ./button cart -->
+                            </div>
+                            <!-- ./data -->
+                        </div>
+                        <!-- product item -->
+                    `;
+                                productsContainer.insertAdjacentHTML('beforeend', productHtml);
+                            });
+                            nextPageUrl = data.nextPage; // Update the next page URL
+                            if (!nextPageUrl) {
+                                loadMoreButton.style.display = 'none'; // Hide the button if no more pages
+                            }
+                        })
+                        .catch(error => console.error('Error:', error));
+                }
             });
         });
     </script>
+
 
 @endsection
