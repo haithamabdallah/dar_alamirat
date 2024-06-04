98bed29cd5390357a382e105dc80a6d02edcb1bc
Haitham AbdAllah <haithamabdallah@gmail.com>
2024-06-04 22:03:41 +0300

    brands page

--- resources/views/themes/theme1/brands.blade.php
+++ resources/views/themes/theme1/brands.blade.php
@@ -1,50 +1,218 @@
-<section class="s-block">
+@extends('themes.theme1.layouts.app')
+
+@section('content')
+
+    <!-- brands Nav -->
+    <nav id="brands-nav">
+        <a href="#section-A" class="active" data-id="A">A</a>
+        <a href="#section-B" class="" data-id="B">B</a>
+        <a href="#section-C" class="" data-id="C">C</a>
+        <a href="#section-D" class="" data-id="D">D</a>
+        <a href="#section-E" class="" data-id="E">E</a>
+        <a href="#section-F" class="" data-id="F">F</a>
+        <a href="#section-G" class="" data-id="G">G</a>
+        <a href="#section-H" class="" data-id="H">H</a>
+        <a href="#section-I" class="" data-id="I">I</a>
+        <a href="#section-J" class="" data-id="J">J</a>
+        <a href="#section-K" class="" data-id="K">K</a>
+        <a href="#section-L" class="" data-id="L">L</a>
+        <a href="#section-M" class="" data-id="M">M</a>
+        <a href="#section-N" class="" data-id="N">N</a>
+        <a href="#section-O" class="" data-id="O">O</a>
+        <a href="#section-P" class="" data-id="P">P</a>
+        <a href="#section-Q" class="" data-id="Q">Q</a>
+        <a href="#section-R" class="" data-id="R">R</a>
+        <a href="#section-S" class="" data-id="S">S</a>
+        <a href="#section-T" class="" data-id="T">T</a>
+        <a href="#section-U" class="" data-id="U">U</a>
+        <a href="#section-V" class="" data-id="V">V</a>
+        <a href="#section-W" class="" data-id="W">W</a>
+        <a href="#section-X" class="" data-id="X">X</a>
+        <a href="#section-Y" class="" data-id="Y">Y</a>
+        <a href="#section-Z" class="" data-id="Z">Z</a>
+    </nav>
+    <!-- ./Brands Nav -->
+
+    <!-- brands items -->
     <div class="pixel-container">
         <div class="wrap">
-            <div class="section-brands">
-                <div class="s-block-title">
-                    <h2>Browse All Brands</h2>
-                    <a href="{{route('brands')}}" class="">View All</a>
+            <section class="s-block" id="section">
+
+                <!-- item -->
+                <div class="section-brands">
+                    <div class="s-brands-list">
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/XspQyl8oKzYAkn4AvIFtB4CNeIMsL8rN3388ojJx.webp') }}" alt="Dior">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/xkn0abLyyebyJQFZeH1GRsm7nu1erFWnpywj0xlB.webp') }}" alt="Cosmo">
+                        </a>
+                    </div>
                 </div>
-                <div class="s-brands-list">
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/XspQyl8oKzYAkn4AvIFtB4CNeIMsL8rN3388ojJx.webp')}}" alt="Dior">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/xkn0abLyyebyJQFZeH1GRsm7nu1erFWnpywj0xlB.webp')}}" alt="Cosmo">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/zYegscEjCJBazDlSJg0Q2CwkNuQWUshb1tA53BuY.webp')}}" alt="LA ROCHE-POSAY">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/QVh6NFjEln9lsiDUhwzAqNYLS9J92TglppKs3J7K.webp')}}" alt="Nars">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/r8PZz6epIKkKIsGfC2xkU6gly4g3SO9iDFjCcep0.webp')}}" alt="elf">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/fKh6tINkceBbymNXTEeqcBC80mXG1gz6V1kHHlno.webp')}}" alt="Cerave">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/g9JuMkkl3Zkye8Wpl7C1lnikeFDlXD7Dlk0AgACC.webp')}}" alt="Essence">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/gBhSACfGCAkalrnO3QE4ZGSPOQ4ZRnvQywI7zZ3A.webp')}}" alt="Cetaphil">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/vfJUW6dyXrsMgTwmSD57Te3ie6enVXsB7RzmvlO6.webp')}}" alt="Spa System">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/wX57sqMy8Lg3sMzUS1MOcq6bJ80SXcXL3g3Pn42p.webp')}}" alt="Garden Olian | Garden Olean">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/y649zLrnh64n5clt7u8FTxdzU9BnpFcQfgJXd4hE.webp')}}" alt="Eucerin">
-                    </a>
-                    <a href="" class="brand-item">
-                        <img class="" src="{{asset('theme1-assets/images/brands/BoWjzF2U0xaCCCAXQbue8UOp8U0IcCFhI8UAL3ng.webp')}}" alt="Bioderma">
-                    </a>
+                <!-- ./item -->
+
+                <!-- item -->
+                <h2 id="section-A">A</h2>
+                <div class="section-brands">
+                    <div class="s-brands-list">
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/XspQyl8oKzYAkn4AvIFtB4CNeIMsL8rN3388ojJx.webp') }}" alt="Dior">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/xkn0abLyyebyJQFZeH1GRsm7nu1erFWnpywj0xlB.webp') }}" alt="Cosmo">
+                        </a>
+                    </div>
                 </div>
-            </div>
+                <!-- ./item -->
+
+                <!-- item -->
+                <h2 id="section-B">B</h2>
+                <div class="section-brands">
+                    <div class="s-brands-list">
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/gBhSACfGCAkalrnO3QE4ZGSPOQ4ZRnvQywI7zZ3A.webp') }}" alt="Cetaphil">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/vfJUW6dyXrsMgTwmSD57Te3ie6enVXsB7RzmvlO6.webp') }}" alt="Spa System">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/wX57sqMy8Lg3sMzUS1MOcq6bJ80SXcXL3g3Pn42p.webp') }}" alt="Garden Olian | Garden Olean">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/y649zLrnh64n5clt7u8FTxdzU9BnpFcQfgJXd4hE.webp') }}" alt="Eucerin">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/BoWjzF2U0xaCCCAXQbue8UOp8U0IcCFhI8UAL3ng.webp') }}" alt="Bioderma">
+                        </a>
+                    </div>
+                </div>
+                <!-- ./item -->
+
+                <!-- item -->
+                <h2 id="section-C">C</h2>
+                <div class="section-brands">
+                    <div class="s-brands-list">
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/XspQyl8oKzYAkn4AvIFtB4CNeIMsL8rN3388ojJx.webp') }}" alt="Dior">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/xkn0abLyyebyJQFZeH1GRsm7nu1erFWnpywj0xlB.webp') }}" alt="Cosmo">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/zYegscEjCJBazDlSJg0Q2CwkNuQWUshb1tA53BuY.webp') }}" alt="LA ROCHE-POSAY">
+                        </a>
+                    </div>
+                </div>
+                <!-- ./item -->
+
+                <!-- item -->
+                <h2 id="section-D">D</h2>
+                <div class="section-brands">
+                    <div class="s-brands-list">
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/XspQyl8oKzYAkn4AvIFtB4CNeIMsL8rN3388ojJx.webp') }}" alt="Dior">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/xkn0abLyyebyJQFZeH1GRsm7nu1erFWnpywj0xlB.webp') }}" alt="Cosmo">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/zYegscEjCJBazDlSJg0Q2CwkNuQWUshb1tA53BuY.webp') }}" alt="LA ROCHE-POSAY">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/y649zLrnh64n5clt7u8FTxdzU9BnpFcQfgJXd4hE.webp') }}" alt="Eucerin">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/BoWjzF2U0xaCCCAXQbue8UOp8U0IcCFhI8UAL3ng.webp') }}" alt="Bioderma">
+                        </a>
+                    </div>
+                </div>
+                <!-- ./item -->
+
+                <!-- item -->
+                <h2 id="section-E">E</h2>
+                <div class="section-brands">
+                    <div class="s-brands-list">
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/XspQyl8oKzYAkn4AvIFtB4CNeIMsL8rN3388ojJx.webp') }}" alt="Dior">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/xkn0abLyyebyJQFZeH1GRsm7nu1erFWnpywj0xlB.webp') }}" alt="Cosmo">
+                        </a>
+                    </div>
+                </div>
+                <!-- ./item -->
+
+                <!-- item -->
+                <h2 id="section-F">F</h2>
+                <div class="section-brands">
+                    <div class="s-brands-list">
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/gBhSACfGCAkalrnO3QE4ZGSPOQ4ZRnvQywI7zZ3A.webp') }}" alt="Cetaphil">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/vfJUW6dyXrsMgTwmSD57Te3ie6enVXsB7RzmvlO6.webp') }}" alt="Spa System">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/wX57sqMy8Lg3sMzUS1MOcq6bJ80SXcXL3g3Pn42p.webp') }}" alt="Garden Olian | Garden Olean">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/y649zLrnh64n5clt7u8FTxdzU9BnpFcQfgJXd4hE.webp') }}" alt="Eucerin">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/BoWjzF2U0xaCCCAXQbue8UOp8U0IcCFhI8UAL3ng.webp') }}" alt="Bioderma">
+                        </a>
+                    </div>
+                </div>
+                <!-- ./item -->
+
+                <!-- item -->
+                <h2 id="section-G">G</h2>
+                <div class="section-brands">
+                    <div class="s-brands-list">
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/XspQyl8oKzYAkn4AvIFtB4CNeIMsL8rN3388ojJx.webp') }}" alt="Dior">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/xkn0abLyyebyJQFZeH1GRsm7nu1erFWnpywj0xlB.webp') }}" alt="Cosmo">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/zYegscEjCJBazDlSJg0Q2CwkNuQWUshb1tA53BuY.webp') }}" alt="LA ROCHE-POSAY">
+                        </a>
+                    </div>
+                </div>
+                <!-- ./item -->
+
+                <!-- item -->
+                <h2 id="section-H">H</h2>
+                <div class="section-brands">
+                    <div class="s-brands-list">
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/XspQyl8oKzYAkn4AvIFtB4CNeIMsL8rN3388ojJx.webp') }}" alt="Dior">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/xkn0abLyyebyJQFZeH1GRsm7nu1erFWnpywj0xlB.webp') }}" alt="Cosmo">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/zYegscEjCJBazDlSJg0Q2CwkNuQWUshb1tA53BuY.webp') }}" alt="LA ROCHE-POSAY">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/y649zLrnh64n5clt7u8FTxdzU9BnpFcQfgJXd4hE.webp') }}" alt="Eucerin">
+                        </a>
+                        <a href="" class="brand-item">
+                            <img class="" src="{{ asset('theme1-assets/images/brands/BoWjzF2U0xaCCCAXQbue8UOp8U0IcCFhI8UAL3ng.webp') }}" alt="Bioderma">
+                        </a>
+                    </div>
+                </div>
+                <!-- ./item -->
+
+            </section>
         </div>
     </div>
-</section>
+    <!-- Brands items -->
+
+
+@endsection
+
+@section('scripts')
+
+@endsection
