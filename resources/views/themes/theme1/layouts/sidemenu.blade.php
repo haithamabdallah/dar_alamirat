6f35e17437ddef4089f9802f4bf65c80cb0af214
Haitham AbdAllah <haithamabdallah@gmail.com>
2024-06-05 14:39:48 +0300

    hide

--- resources/views/themes/theme1/layouts/sidemenu.blade.php
+++ resources/views/themes/theme1/layouts/sidemenu.blade.php
@@ -1,4 +1,4 @@
-<div class="offcanvas offcanvas-start show" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
     <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasExampleLabel">Main Menu</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
