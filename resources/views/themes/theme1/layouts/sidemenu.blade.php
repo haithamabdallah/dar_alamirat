ad82737d5997296d6e6b167d99a7d235055e8608
Ali Reda <alyredagomaa@gmail.com>
2024-06-27 22:28:33 +0300

    edit subcategory in sidemenu till 4 childs

--- resources/views/themes/theme1/layouts/sidemenu.blade.php
+++ resources/views/themes/theme1/layouts/sidemenu.blade.php
@@ -10,41 +10,77 @@
                     <a class="d-flex align-items-center" data-bs-toggle="modal" data-bs-target="#loginEmail">
                         <i class="icon sicon-user"></i>
                         <span class="d-flex flex-column">
-                                <p>My Account</p>
-                                <span>Login</span>
-                            </span>
+                            <p>My Account</p>
+                            <span>Login</span>
+                        </span>
                     </a>
                 </li>
             @endguest
             @auth
                 <li>
-                    <a href="{{ route('user.profile',auth()->user()->id) }}" class="d-flex align-items-center">
+                    <a href="{{ route('user.profile', auth()->user()->id) }}" class="d-flex align-items-center">
                         <i class="icon sicon-user"></i>
                         <span class="d-flex flex-column">
-                                <p>My Account</p>
-                                <span>{{ auth()->user()->FullName }}</span>
-                            </span>
+                            <p>My Account</p>
+                            <span>{{ auth()->user()->FullName }}</span>
+                        </span>
                     </a>
                 </li>
             @endauth
         </ul>
 
         <ul class="nav">
-            @foreach(defaultCategory() as $category)
-                @if($category->childes->isNotEmpty())
+            @foreach (defaultCategory() as $category)
+                @if ($category->childes->isNotEmpty())
                     <li class="nav__item">
-                        <a class="nav__link" href="{{route('category.products' , $category->id)}}">{{$category->name}}<i class="fas fa-chevron-right"></i></a>
+                        <a class="nav__link"
+                            href="{{ route('category.products', $category->id) }}">{{ $category->name }}<i
+                                class="fas fa-chevron-right"></i></a>
                         <ul class="nav__sub">
-                            @foreach($category->childes as $child)
-                                <li class="nav__item">
-                                    <a class="nav__link" href="{{route('category.products' , $category->id)}}">{{$child->name}}</a>
-                                </li>
+                            @foreach ($category->childes as $child)
+                                @if ($child->childes->isNotEmpty())
+                                    <li class="nav__item">
+                                        <a class="nav__link"
+                                            href="{{ route('category.products', $child->id) }}">{{ $child->name }}<i
+                                                class="fas fa-chevron-right"></i></a>
+                                        <ul class="nav__sub">
+                                            @foreach ($child->childes as $child2)
+                                                @if ($child2->childes->isNotEmpty())
+                                                    <li class="nav__item">
+                                                        <a class="nav__link"
+                                                            href="{{ route('category.products', $child2->id) }}">{{ $child2->name }}<i
+                                                                class="fas fa-chevron-right"></i></a>
+                                                        <ul class="nav__sub">
+                                                            @foreach ($child2->childes as $child3)
+                                                                <li class="nav__item">
+                                                                    <a class="nav__link"
+                                                                        href="{{ route('category.products', $child3->id) }}">{{ $child3->name }}</a>
+                                                                </li>
+                                                            @endforeach
+                                                        </ul>
+                                                    </li>
+                                                @else
+                                                    <li class="nav__item">
+                                                        <a class="nav__link"
+                                                            href="{{ route('category.products', $child2->id) }}">{{ $child2->name }}</a>
+                                                    </li>
+                                                @endif
+                                            @endforeach
+                                        </ul>
+                                    </li>
+                                @else
+                                    <li class="nav__item">
+                                        <a class="nav__link"
+                                            href="{{ route('category.products', $child->id) }}">{{ $child->name }}</a>
+                                    </li>
+                                @endif
                             @endforeach
                         </ul>
                     </li>
                 @else
                     <li class="nav__item">
-                        <a class="nav__link" href="{{route('category.products' , $category->id)}}">{{$category->name}}</a>
+                        <a class="nav__link"
+                            href="{{ route('category.products', $category->id) }}">{{ $category->name }}</a>
                     </li>
                 @endif
             @endforeach
