1128a677fad34ffbffbf467f01685e7e5d500817
Ali Reda <alyredagomaa@gmail.com>
2024-06-28 19:58:39 +0300

    fix : invoice mail

--- app/Mail/InvoiceMail.php
+++ app/Mail/InvoiceMail.php
@@ -38,7 +38,7 @@ class InvoiceMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.emails.invoice',
+            view: 'emails.invoice',
         );
     }
 
