28a2b54b79e460e66b529c170316a3990cf0bf9d
Haitham AbdAllah <haithamabdallah@gmail.com>
2024-07-02 05:33:31 +0300

    cookies blade file

--- resources/views/themes/theme1/index.blade.php
+++ resources/views/themes/theme1/index.blade.php
@@ -112,31 +112,9 @@
     </section>
     <!-- ./Brands -->
 
-    <section id="newsletterPopup" class="hide">
-        <div class="newContent">
-            <a href="javascript:;" class="closeNews close-btn"><i class="fa-solid fa-xmark"></i></a>
-            <div class="newsImage">
-                <img src="{{ asset('theme1-assets/images/newsletter/01.jpg') }}" alt="">
-            </div>
-            <div id="subscription">
-                <h3>Sign Up For</h3>
-                <h1>25% OFF</h1>
-                <p>Subscribe to our newsletter for exclusive beauty tips, new product launches, and special offers.</p>
-                <form id="newsletter-form" action="{{ route('subscribe') }}" method="POST">
-                    @csrf
-                    <input type="email" name="email" placeholder="Enter your email address" required>
-                    <button type="submit">Subscribe Now</button>
-                </form>
-                <a class="closeNews" href="javascript:;">No, thanks</a>
-                <p>By entering, you agree to the <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a></p>
-            </div>
-            <div id="NewsSuccess" class="hide">
-                <h3>Thank you for subscribing!</h3>
-                <p>You will now receive our newsletter with exclusive beauty tips, new product launches, and special offers.</p>
-            </div>
-        </div>
-    </section>
+    @include('themes.theme1.partials.newsletters')
 
+    @include('themes.theme1.partials.cookies')
 
     {{--  --}}
     {{-- @section('scripts')
@@ -251,5 +229,24 @@
                 });
             });
         });
+    </script>
+
+    <script>
+
+        const newsletterPopup = document.getElementById('cookiesPopup');
+
+        setTimeout(() => {
+            newsletterPopup.classList.remove('hide');
+        }, 2000);
+
+        const closeButtons = document.querySelectorAll('.closeNews'); // Select all elements with class "closeNews"
+        const popup = document.getElementById("cookiesPopup");
+
+        closeButtons.forEach(closeButton => {
+            closeButton.addEventListener('click', function() {
+                popup.classList.add('hide'); // Add class "hide" to the popup element
+            });
+        });
+
     </script>
 @endsection
