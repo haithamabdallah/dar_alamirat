93a7c93bcf90a160f915ccff4ff3b2ae95847db2
Ali Reda <alyredagomaa@gmail.com>
2024-07-04 16:56:07 +0300

    edit coupon responsed text at arabic

--- resources/views/themes/theme1/cart-page-scripts.blade.php
+++ resources/views/themes/theme1/cart-page-scripts.blade.php
@@ -130,33 +130,6 @@
         }
     }
 
-    function applyCoupon() {
-        let couponCode = $('#coupon-code').val();
-        axios.post('{{ route("coupon.check") }}', {
-            'coupon_code': couponCode
-        }).then((response) => {
-            if (response.data.status === 'success') {
-                console.log(response.data);
-
-                let coupon = response.data.coupon;
-                let couponDetails = `<b> Minimum Purchase Limit:</b> ${coupon.min_purchase_limit} {!! $currency !!}<br>`;
-                if (coupon.discount_type == 'flat') {
-                    couponDetails += `<b> Discount Value:</b> ${coupon.discount_value} {!! $currency !!}<br>`;
-                } else {
-                    couponDetails += `<b> Discount Value:</b> ${coupon.discount_value} % of final total <br> `;
-                }
-                couponDetails += `<b>The coupon will not be applied to products that already have a discount</b>`;
-                $('#coupon-details').html(couponDetails);
-                $('#coupon-details-div').show();
-            } else {
-                $('#coupon-details-div').hide();
-                $('#invalid-coupon').show();
-                $('#invalid-coupon').hide(2000);
-                console.log(response.data);
-            }
-        }).catch((error) => {
-            $('#coupon-details-div').hide();
-            console.log(error);
-        });
-    }
 </script>
+
+@include('themes.theme1.shared-scripts.apply-coupon')
--- resources/views/themes/theme1/checkout/scripts/script.blade.php
+++ resources/views/themes/theme1/checkout/scripts/script.blade.php
@@ -356,36 +356,6 @@
     // Initial render for the address list
     renderAddressList();
 
-
-    function applyCoupon() {
-        let couponCode = $('#coupon-code').val();
-        axios.post('{{ route('coupon.check') }}', {
-            'coupon_code': couponCode
-        }).then((response) => {
-            if (response.data.status === 'success') {
-                console.log(response.data);
-                let coupon = response.data.coupon;
-                let couponDetails = `<b>  {{ __("Minimum Purchase Limit") }}  :</b> ${coupon.min_purchase_limit} {!! $currency !!}<br>`;
-                if (coupon.discount_type == 'flat') {
-                    couponDetails += `<b>  {{ __("Discount Value") }}  :</b> ${coupon.discount_value} {!! $currency !!}<br>`;
-                } else {
-                    couponDetails += `<b>  {{ __("Discount Value") }}  :</b> ${coupon.discount_value} % of final total <br> `;
-                }
-                couponDetails += `<b> {{ __("The coupon will not be applied to products that already have a discount") }}  </b>`;
-                $('#coupon-details').html(couponDetails);
-                $('#coupon-details-div').show();
-            } else {
-                $('#coupon-details-div').hide();
-                $('#invalid-coupon').show();
-                $('#invalid-coupon').hide(2000);
-                console.log(response.data);
-            }
-        }).catch((error) => {
-            $('#coupon-details-div').hide();
-            console.log(error);
-        });
-    }
-
     $(document).ready(function() {
         if ($('#coupon-code').val() != '') {
             $('#coupon-code-btn')[0].click();
@@ -393,3 +363,6 @@
         }
     });
 </script>
+
+@include('themes.theme1.shared-scripts.apply-coupon')
+
