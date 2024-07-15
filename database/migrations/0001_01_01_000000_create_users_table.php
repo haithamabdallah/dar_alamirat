fcede5e39c83346c414ad2772539c7968621ef01
Ali Reda <alyredagomaa@gmail.com>
2024-07-15 21:43:43 +0300

    !!!! Commands required !!! ( edit users table  )

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,6 +25,12 @@ return new class extends Migration
             $table->timestamps();
         });
 
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('last_name', function($table) {
+                $table->string('avatar')->nullable();
+            });
+        });
+
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
